084cbc55d86e93a9446ecd648cee205a56f2786a
LJ <Lawrence Gray>
2016-07-09 17:25:25 +0100

    second attempt at smooth scolling just need to check it on the server

--- src/script/script.js
+++ src/script/script.js
@@ -56,3 +56,31 @@ $(document).ready(function(){
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
     window.location.hash = this.hash;
 */
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified 
+      if($(hash).length){
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+      } else {
+        window.location.hash = hash;
+      }
+
+    } // End if
+  });
+});
