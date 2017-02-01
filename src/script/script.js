46061a16a67b378b8bff69a54b23595e9235cdd3
Lawrence Gray <ljg2305@gmail.com>
2017-02-01 20:31:03 +0000

    sorts out different sized images in gallery

--- src/script/script.js
+++ src/script/script.js
@@ -35,43 +35,30 @@ $(document).ready(function(){
   });
 
 
-// Normalize Carousel Heights - pass in Bootstrap Carousel items.
-$.fn.carouselHeights = function() {
-
-    var items = $(this), //grab all slides
+function carouselNormalization() {
+  var items = $('#total_refurb_carousel .item'), //grab all slides
         heights = [], //create empty array to store height values
         tallest; //create variable to make note of the tallest slide
 
-    var normalizeHeights = function() {
+    if( ! items.length )
+        return;
 
-        items.each(function() { //add heights to array
-            heights.push($(this).height()); 
-        });
-        tallest = Math.max.apply(null, heights); //cache largest value
-        items.each(function() {
-            $(this).css('min-height',tallest + 'px');
-        });
+    function normalizeHeights() {
+      items.each( function() { //add heights to array
+        heights.push( $(this).height() ); 
+      } );
+      tallest = Math.max.apply( null, heights ); //cache largest value
+      items.css( 'min-height', tallest + 'px' );
     };
-
     normalizeHeights();
 
-    $(window).on('resize orientationchange', function () {
-        //reset vars
-        tallest = 0;
-        heights.length = 0;
+    $( window ).on( 'resize orientationchange', function () {
+      tallest = 0, heights.length = 0; //reset vars
+      items.css( 'min-height', '0' ); //reset min-height
+      normalizeHeights(); //run it again 
+    } );
+}
 
-        items.each(function() {
-            $(this).css('min-height','0'); //reset min-height
-        }); 
-        normalizeHeights(); //run it again 
-    });
-
-};
-
-jQuery(function($){
-
-    $(window).on('load', function(){
-        $('#total_refurb_carousel .item').carouselHeights();
-    });
-
+$(documnet).ready(function(){
+    carouselNormalization()
 });
