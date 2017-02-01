4d4d223175e2e385ba44c1fd2f6391d95772deec
Lawrence Gray <ljg2305@gmail.com>
2017-02-01 20:47:40 +0000

    sorts out different sized images in gallery

--- src/script/script.js
+++ src/script/script.js
@@ -36,28 +36,37 @@ $(document).ready(function(){
 
 
 function carouselNormalization() {
-  var items = $('#total_refurb_carousel .item'), //grab all slides
-        heights = [], //create empty array to store height values
-        tallest; //create variable to make note of the tallest slide
+  var items   = $('#total_refurb_carousel .item'),              // grab all the slides
+      heights = [],                                   // array to store heights
+      tallest;                                        // tallest slide
 
-    if( ! items.length )
-        return;
-
+  if (items.length) {
     function normalizeHeights() {
-      items.each( function() { //add heights to array
-        heights.push( $(this).height() ); 
-      } );
-      tallest = Math.max.apply( null, heights ); //cache largest value
-      items.css( 'min-height', tallest + 'px' );
+      items.each(function() {
+        heights.push($(this).height());               // add each slide's height
+      });                                             // to the array
+
+      tallest = Math.max.apply(null, heights);        // find the largest height
+
+      items.each(function() {
+        $(this).css('min-height', tallest + 'px');    // set each slide's minimum
+      });                                             // height to the largest
     };
+
     normalizeHeights();
 
-    $( window ).on( 'resize orientationchange', function () {
-      tallest = 0, heights.length = 0; //reset vars
-      items.css( 'min-height', '0' ); //reset min-height
-      normalizeHeights(); //run it again 
-    } );
+    $(window).on('resize orientationchange', function() {
+      tallest = 0, heights.length = 0;               // reset the variables
+
+      items.each(function() {
+        $(this).css('min-height', '0');              // reset each slide's height
+      });
+
+      normalizeHeights();                            // run it again
+    });
+  }
 }
+
 
 window.onload = function() {
   carouselNormalization();
